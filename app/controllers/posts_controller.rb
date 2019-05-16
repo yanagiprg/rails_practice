068f0d0fa93ed5766b4ebd2260e0881ffaa65baa
yanagi_prg <takayanagi.prg@gmail.com>
2019-05-17 02:39:34 +0900

    adding Comment function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,9 @@ def update
 end
 
 def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
 end
 
 private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_164201) do
+ActiveRecord::Schema.define(version: 2019_05_16_160817) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
