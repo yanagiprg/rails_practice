087234043ddacae555044182f476a23b877c710c
yanagi_prg <takayanagi.prg@gmail.com>
2019-05-21 01:32:12 +0900

    change root from welcome#index to posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'welcome#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
